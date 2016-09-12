943aa1dca8d74fed5a9e56de5567d87a7bcd9d07
ponsfrilus <ponsfrilus@gmail.com>
2016-09-12 17:53:28 +0200

    Dumps use the defined user instead of root

--- run.sh
+++ run.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # https://hub.docker.com/_/mysql/
 # https://github.com/besnik/tutorials/tree/master/docker-mysql
+# Quick and dirty way to execute mysql command in the container:
+#   docker exec -it mysql_docker mysql -u root -pmyverysecurepassword -e "show databases;";
+#set -e -x
 
 # A POSIX variable
 OPTIND=1 # Reset in case getopts has been used previously in the shell.
 
-
-#set -e -x
-
+# Script's variables
 : ${DOCKER_MYSQL_NAME:=mysql_docker}
 : ${DOCKER_MYSQL_DB_NAME:=mysql_docker}
 : ${DOCKER_MYSQL_ROOT_PASSWORD:=myverysecurepassword}
 : ${DOCKER_MYSQL_USER:=mysql_docker}
 : ${DOCKER_MYSQL_USER_PASSWORD:=myverysecurepassword}
 : ${MYSQL_DIRECTORY_PATH:=$PWD}
+
 # Make the MYSQL directory
+# TODO: at some point you may want to remove the mysql (or at least the data)
+#       directory. This is important if you change the variable, e.g. passwords.
 mkdir -p $MYSQL_DIRECTORY_PATH/mysql/{conf.d,initdb.d,data,dumps}
 
 # Show help function
@@ -29,42 +33,52 @@ showHelp() {
     echo "    - i   : interactively connect to the $DOCKER_MYSQL_NAME container"
 }
 
-# testing input params
+# Show help in case of no opt
 if [[ $# -eq 0 ]] ; then
     showHelp
     exit 0
 fi
 
+# Run the docker container
 run() {
-    # Run the docker container
     docker run \
         --name $DOCKER_MYSQL_NAME \
         -e MYSQL_ROOT_PASSWORD=$DOCKER_MYSQL_ROOT_PASSWORD \
         -e MYSQL_DATABASE=$DOCKER_MYSQL_DB_NAME \
         -e MYSQL_USER=$DOCKER_MYSQL_USER \
         -e MYSQL_PASSWORD=$DOCKER_MYSQL_USER_PASSWORD \
+        -v $MYSQL_DIRECTORY_PATH/mysql/data:/var/lib/mysql \
+        -v $MYSQL_DIRECTORY_PATH/mysql/initdb.d:/docker-entrypoint-initdb.d \
         -d mysql:5
-
         #-v $MYSQL_DIRECTORY_PATH/mysql/conf.d:/etc/mysql/conf.d \
-        #-v $MYSQL_DIRECTORY_PATH/mysql/initdb.d:/docker-entrypoint-initdb.d \
-        #-v $MYSQL_DIRECTORY_PATH/mysql/data:/var/lib/mysql \
 }
 
+# Dump the DOCKER_MYSQL_DB_NAMEv using DOCKER_MYSQL_USER and DOCKER_MYSQL_USER_PASSWORD
 dump() {
-    # Mysql Dump
-    DUMP_DATE=$(date +"%Y%m%d%H%m%S")
+    DUMP_DATE=$(date +"%Y%m%d%H%M%S")
     DUMP_FILE=$MYSQL_DIRECTORY_PATH/mysql/dumps/$DUMP_DATE\_$DOCKER_MYSQL_DB_NAME\_dump.sql
+    # The Root Way
+    #docker exec $DOCKER_MYSQL_NAME \
+    #    sh -c 'exec mysqldump --databases $MYSQL_DATABASE -uroot -p"$MYSQL_ROOT_PASSWORD"'> $DUMP_FILE
+    # CLI: docker exec mysql_docker sh -c 'exec mysqldump --databases mysql_docker -uroot -p"$MYSQL_ROOT_PASSWORD"' > dump.sql
+
+    # The user way
     docker exec $DOCKER_MYSQL_NAME \
-        sh -c 'exec mysqldump --databases $MYSQL_DATABASE -uroot -p"$MYSQL_ROOT_PASSWORD"'> $DUMP_FILE
-    # docker exec mysql_docker sh -c 'exec mysqldump --databases mysql_docker -uroot -p"$MYSQL_ROOT_PASSWORD"' > dump.sql
+        sh -c 'exec mysqldump --databases $MYSQL_DATABASE -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"'> $DUMP_FILE
+
     head -5 $DUMP_FILE
     echo ""
     echo "Full dump: $DUMP_FILE"
+
+    # NOTE: if error:
+    #       mysqldump: Got error: 2002: Can't connect to local MySQL server
+    #       through soket '/var/run/mysqld/mysqld.sock' (2) when trying to connect
+    #       => wait for a few seconds, mysql service is not fully started in the
+    #       container.
 }
 
-
+# Ensure the mysql's docker is stopped and removed
 killc() {
-    # Ensure the mysql's docker is stopped and removed
     StateRunning=$(docker inspect $DOCKER_MYSQL_NAME | jq '.[0] | .State.Running')
     StateStatus=$(docker inspect $DOCKER_MYSQL_NAME | jq '.[0] | .State.Status')
 
@@ -80,15 +94,15 @@ killc() {
     fi
 }
 
+# Interactive Shell
 interactive() {
-    # Interactive Shell
 
     # TODO docker inspect $DOCKER_MYSQL_NAME and run it if needed
 
     docker exec -it $DOCKER_MYSQL_NAME bash
 }
 
-# menu cases
+# Menu cases
 # --h: Help
 # --d: Dump
 # --r: Run
